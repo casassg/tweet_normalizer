6207dce283399206dd2260a9d6c898b621638cf1
Gerard Casas Saez <casassg@gmail.com>
2017-04-11 21:17:00 -0600

    consume tweets as a group

--- eventconsumer.py
+++ eventconsumer.py
@@ -13,10 +13,16 @@ bootstrap_servers = os.environ.get('KAFKA_SERVERS', 'localhost:9092').split(',')
 
 
 def main():
-    consumer = KafkaConsumer('raw_topics', group_id=event, value_deserializer=lambda m: json.loads(m.decode('ascii')))
+    consumer = KafkaConsumer('raw_tweets', group_id=event, value_deserializer=lambda m: json.loads(m.decode('utf-8')))
 
-    for tweet in consumer:
-        logging.info('Tweet: %s' % tweet)
+    for message in consumer:
+        tweet = json.loads(message.value)
+        if 'id' not in tweet:
+            logging.error('ERRONIOUS TWEET: %s' % tweet)
+            continue
+        logging.info("Tweet received: %s:%d:%d: key=%s tweet_id=%s" % (message.topic, message.partition,
+                                                    message.offset, message.key,
+                                                    tweet['id']))
 
 
 if __name__ == "__main__":
