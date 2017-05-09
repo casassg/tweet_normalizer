fc1b5463d8cc7279b5d5a9a75ca32cb5e9968279
Gerard Casas Saez <casassg@gmail.com>
2017-05-08 21:00:33 -0600

    change to confluent kafka library

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,15 @@
 FROM python:2.7-alpine
+
 RUN apk add --no-cache gcc musl-dev
+RUN sed -i -e 's/v3\.4/edge/g' /etc/apk/repositories  && \
+    apk --no-cache add alpine-sdk librdkafka-dev
 
 ADD requirements.txt /
 RUN pip install appdirs --upgrade
 RUN pip install -r requirements.txt
+RUN apk del alpine-sdk && \
+    rm -rf /root/cache/*
+
 ADD model.py /
 ADD tweetparser.py /
 
--- tweetparser.py
+++ tweetparser.py
@@ -1,9 +1,9 @@
 import logging
 import os
-
-import time
+import sys
 import ujson
-from kafka import KafkaConsumer
+
+from confluent_kafka import Consumer, KafkaError, KafkaException
 
 EVENT_KEY = os.environ.get('EVENT_KEY', '')
 
@@ -13,36 +13,59 @@ TOKENS = list(filter(None, os.environ.get('TOKENS', '').split(',')))
 
 assert TOKENS, 'Tokens can\'t be empty'
 
-KAFKA_SERVER = os.environ.get('KAFKA_SERVERS', 'localhost:9092').split(',')
+KAFKA_SERVER = os.environ.get('KAFKA_SERVERS', 'localhost:9092')
 
 
 def main(save):
-    consumer = KafkaConsumer('raw_tweets', group_id=EVENT_KEY, bootstrap_servers=KAFKA_SERVER,
-                             value_deserializer=lambda m: ujson.loads(m.decode('utf-8')))
+    conf = {'bootstrap.servers': KAFKA_SERVER,
+            'group.id': EVENT_KEY,
+            'session.timeout.ms': 6000,
+            'default.topic.config': {'auto.offset.reset': 'smallest'}
+            }
+    c = Consumer(**conf)
 
-    for message in consumer:
+    def print_assignment(consumer, partitions):
+        logging.info('Assignment: %s' % partitions)
 
-        # first = time.time()
-        # start_time = time.time()
+    # Subscribe to topics
+    c.subscribe(['raw_tweets', ], on_assign=print_assignment)
 
-        tweet = ujson.loads(message.value)
-        # logging.info('Parse JSON: %s' % (time.time() - start_time))
-        if 'id' not in tweet:
-            logging.error('ERRONIOUS TWEET: %s' % tweet)
-            continue
-        # start_time = time.time()
-        if any(token in tweet['text'] for token in TOKENS):
-            # logging.info('Filtered tweet: %s' % (time.time() - start_time))
+    try:
+        msg_count = 0
+        while True:
+            msg = c.poll()
+            if msg is None:
+                continue
+            if msg.error():
+                # Error or event
+                if msg.error().code() == KafkaError._PARTITION_EOF:
+                    # End of partition event
+                    logging.error('%% %s [%d] reached end at offset %d' % (msg.topic(), msg.partition(), msg.offset()))
+                elif msg.error():
+                    # Error
+                    raise KafkaException(msg.error())
+            else:
+                tweet = ujson.loads(ujson.loads(msg.value()))
+                if 'text' not in tweet:
+                    logging.info('Internal message: %s' % tweet)
+                    continue
+                msg_count += 1
+                # Proper message
 
-            logging.info("Tweet accepted: %s:%d:%d: key=%s tweet_id=%s" % (message.topic, message.partition,
-                                                                           message.offset, message.key,
-                                                                           tweet['id']))
-            # start_time = time.time()
-            save(tweet, EVENT_KEY, TOKENS)
-            # logging.info('Tweet Saved: %s' % (time.time() - start_time))
-            # Open file to mark that we are healthy (This way K8s knows we are working here)
-            open('/tmp/healthy', 'a').close()
-            # logging.info('Total elapsed time : %s' % (time.time() - first))
+                if any(token in tweet['text'] for token in TOKENS):
+                    logging.info('Tweet accepted: %s:%d:%d: key=%s tweet_id=%s' %
+                                 (msg.topic(), msg.partition(), msg.offset(),
+                                  str(msg.key()), tweet['id']))
+                    save(tweet, EVENT_KEY, TOKENS)
+
+            if msg_count == 1:
+                open('/tmp/healthy', 'a').close()
+
+    except KeyboardInterrupt:
+        sys.stderr.write('%% Aborted by user\n')
+    finally:
+        # Shut down c
+        c.close()
 
 
 if __name__ == "__main__":
@@ -54,7 +77,7 @@ if __name__ == "__main__":
 
     logging.info('Event: %s' % EVENT_KEY)
     logging.info('Tracking keywords: %s' % ','.join(TOKENS))
-    logging.info('Kafka servers: %s' % ','.join(KAFKA_SERVER))
+    logging.info('Kafka servers: %s' % KAFKA_SERVER)
     logging.info('Connecting to Cassandra...')
     logging.info('Start stream track')
     if not TOKENS:
